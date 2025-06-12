1e80d71e58e399f061d036fbb09aff37816ede90
sandra Alex <sandra5617alex@gmail.com>
2025-06-12 11:47:16 +0530

    redirected to onboarding

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-'use client'; // Tells Next.js this page uses client-side interactivity
-
-import { useState } from 'react';
-import { useRouter } from 'next/navigation'; // For navigation between pages
-import Image from 'next/image'; // To load images from the public folder
-import './page.css'
-
-
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-  <main className="onboarding-container">
-    <div className="banner-container">
-      <Image
-        src="/images/bannerpublic.png" 
-        alt="Banner"
-        width={1392}
-        height={200}
-        className="banner-image"
-      />
-    </div>
-
-    {/* <h1 className="title">
-      👋 Welcome to Court Click
-    </h1> */}
-    {/* <p className="subtitle">
-      Streamlining your legal workflow with instant court updates.
-    </p> */}
-
-    </main>
-  );
+  redirect('/onboarding');
 }
